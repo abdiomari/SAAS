56b6401fdba6967391d7b4a78af51c792079c99e
Abdi Omari <abdikipkorir@gmail.com>
2026-05-06 10:40:26 +0300

    Fresh Start

---
+++ src/core/views.py
@@ -0,0 +1,18 @@
+import pathlib
+from django.shortcuts import render
+from django.http import HttpResponse
+from visits.models import PageVisit
+
+
+def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title,
+        "queryset" : page_qs.count(),
+        "total_visit_count": qs.count()
+    }
+    html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template, my_context)
